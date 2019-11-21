378aecd2b9feb0226f6da00a2e741c35ae3478bf
Jessica Nguyen <jessicanguyen320@gmail.com>
2019-11-20 16:11:40 -0800

    exporting & importing shop/root/route

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const bodyParser = require("body-parser");
+const shopRoutes = require("./routes/shop");
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
+
+// The shop routes
+app.use(shopRoutes);
 
 // Listener
 http.listen(3000, () => {
